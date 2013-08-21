8cf017203615a0d18f1e9bf2365f2f85b24eaa85
Cesar Schneider <cesschneider@gmail.com>
2013-08-21 13:32:26 -0300

    Atualizado formulario de contato para enviar email

--- projects/roadapps-website/send.php
+++ projects/roadapps-website/send.php
@@ -6,5 +6,7 @@ $_POST['userAgent'] = $_SERVER['HTTP_USER_AGENT'];
 $_POST['timestamp'] = date('Y-m-d H:i:s');
 
 error_log(json_encode($_POST) ."\n", 3, 'cotacao.json');
+mail('cesar@roadapps.net', 'Contato site', print_r($_POST, true));
+mail('mauricio@roadapps.net', 'Contato site', print_r($_POST, true));
 
 ?>
